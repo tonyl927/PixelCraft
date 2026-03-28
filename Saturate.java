747907065b85da68c21fb9e86992369b239d67c4
tonyl927 <tonyl@my.yorku.ca>
2026-03-28 18:28:40 -0400

    added saturate

---
+++ Saturate.java
@@ -0,0 +1,6 @@
+public class Saturate {
+    
+}
+
+// increase the highest colour out of rbg by a factor of 1.5, while keeping the other two colours the same.
+//or also decrease the other 2 by the same amount

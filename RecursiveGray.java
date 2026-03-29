72188581c8522edc7c7817f2a47e278e99130cef
Teniolayusuf <yusuf007@my.yorku.ca>
2026-03-29 02:17:49 -0400

    Update RecursiveGray.java

--- RecursiveGray.java
+++ RecursiveGray.java
@@ -1,1 +1,36 @@
+import java.awt.image.BufferedImage;
 
+public class RecursiveGray extends Converter {
+
+    @Override
+    protected void processImage(BufferedImage input, BufferedImage output) {
+        processRecursive(input, output, 0);
+    }
+
+    private void processRecursive(BufferedImage input, BufferedImage output, int index) {
+
+        int width = input.getWidth();
+        int height = input.getHeight();
+
+        int total = width * height;
+
+        // base case
+        if (index >= total)
+            return;
+
+        // convert index -> (x, y)
+        int x = index % width;
+        int y = index / width;
+        int pixel = input.getRGB(x, y);
+        ARGB color = new ARGB(pixel);
+
+        int gray = (color.red + color.green + color.blue) / 3;
+
+        ARGB newColor = new ARGB(color.alpha, gray, gray, gray);
+        output.setRGB(x, y, newColor.toInt());
+
+        // next pixel
+        processRecursive(input, output, index + 1);
+
+    }
+}

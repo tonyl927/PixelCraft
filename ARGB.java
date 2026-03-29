8d7e34040e7c0f0d920b94621ab99a29b58d71cf
Teniolayusuf <yusuf007@my.yorku.ca>
2026-03-29 02:10:20 -0400

    Update ARGB.java

--- ARGB.java
+++ ARGB.java
@@ -1,1 +1,22 @@
+public class ARGB {
 
+    public int alpha, red, green, blue;
+
+    public ARGB(int pixel) {
+        this.alpha = (pixel >> 24) & 255;
+        this.red = (pixel >> 16) & 255;
+        this.green = (pixel >> 8) & 255;
+        this.blue = pixel & 255;
+    }
+
+    public ARGB(int a, int r, int g, int b) {
+        this.alpha = a;
+        this.red = r;
+        this.green = g;
+        this.blue = b;
+    }
+
+    public int toInt() {
+        return (alpha << 24) | (red << 16) | (green << 8) | blue;
+    }
+}

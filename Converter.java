776f7bbfcbbd7e49a75e8d8ec8f7093b4422b77f
Teniolayusuf <yusuf007@my.yorku.ca>
2026-03-29 20:55:42 -0400

    Converter.java

--- Converter.java
+++ Converter.java
@@ -1,14 +1,29 @@
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 
-public class Converter {
-    
-    
-    public Converter(){
-
-    }
+public abstract class Converter {
 
     public void convert(String inputFileName, String outputFileName) throws IOException {
         // This method should be overridden by subclasses to implement specific conversion logic.
         // For example, a GrayScale converter would read the input image, convert it to grayscale, and save it as the output image.
+
+        BufferedImage inputImage = ImageIO.read(new File(inputFileName));
+
+        int width = inputImage.getWidth();
+        int height = inputImage.getHeight();
+
+        BufferedImage outputImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+
+        processImage(inputImage, outputImage);
+
+        ImageIO.write(outputImage, "PNG", new File(outputFileName));
+
     }
+
+    protected abstract void processImage(BufferedImage input, BufferedImage output);
 }
+
+
